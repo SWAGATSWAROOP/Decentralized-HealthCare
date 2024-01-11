bad15a7cea5ba8a211728c6e42d9a7849f8ceeb2
Swagat <swagatswaroop@gmail.com>
2024-01-12 03:06:00 +0530

    Add OrgDocModel to backend/src/models/orgdocter.js

---
+++ backend/src/models/orgdocter.js
@@ -0,0 +1,103 @@
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const orgDocSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    phoneno: {
+      type: String,
+      trim: true,
+    },
+    address: {
+      type: String,
+      trim: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    profilephoto: {
+      type: String,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Hashing the password before saving
+orgDocSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// ForChecking Password
+orgDocSchema.methods.checkPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// generateAccessToken
+orgDocSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      name: this.name,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// generateRefreshToken
+orgDocSchema.methods.generateAccessToken = () => {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const OrgDocModel = mongoose.model("OrgDoc", orgDocSchema);
