b92fc11bf21b7796eec17509bb9faefd5aefcc82
Swagat <swagatswaroop@gmail.com>
2024-01-12 01:28:44 +0530

    Add new dependencies to backend package.json

---
+++ backend/src/utils/Cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+export const uploadProfilePhoto = async (localPath) => {
+  try {
+    // if path is not present
+    if (!localPath) return null;
+    // upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localPath, {
+      resource_type: "auto",
+    });
+    // getting url of the file
+    console.log("File is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    // remove the locally saved temporary file from the server as the operation got failed
+    fs.unlinkSync(localPath);
+  }
+};
+
+cloudinary.uploader.upload(
+  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) {
+    console.log(result);
+  }
+);
