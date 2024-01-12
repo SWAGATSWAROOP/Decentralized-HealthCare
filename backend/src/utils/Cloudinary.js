8d8534e596f219464ca5459bb0179ba09f23cb0b
Swagat <swagatswaroop@gmail.com>
2024-01-12 20:31:21 +0530

    Remove unnecessary code in Cloudinary.js

--- backend/src/utils/Cloudinary.js
+++ backend/src/utils/Cloudinary.js
@@ -25,11 +25,3 @@ export const uploadProfilePhoto = async (localPath) => {
     fs.unlinkSync(localPath);
   }
 };
-
-cloudinary.uploader.upload(
-  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" },
-  function (error, result) {
-    console.log(result);
-  }
-);
