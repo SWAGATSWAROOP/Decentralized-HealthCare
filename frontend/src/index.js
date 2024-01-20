4e7ec33f65a4f2ea3919534d8c456cf3d6b0b679
Swagat <swagatswaroop@gmail.com>
2024-01-21 02:26:24 +0530

    Refactor react-router-dom imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './style.css';
 import Home from './views/home';
