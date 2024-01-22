232a77a66f03c6b5a64339e88734be1ef25e398c
Swagat <swagatswaroop@gmail.com>
2024-01-23 00:28:01 +0530

    Remove unused import statement

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 
