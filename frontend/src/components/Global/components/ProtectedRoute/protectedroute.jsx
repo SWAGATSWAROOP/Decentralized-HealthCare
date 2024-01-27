25f6044a2a315332dc6c005b8439523ca2c8dd59
Swagat <swagatswaroop@gmail.com>
2024-01-27 11:01:07 +0530

    Add dispatch and useEffect to ProtectedRoute component Remove redundant code in Dashboard component

--- frontend/src/components/Global/components/ProtectedRoute/protectedroute.jsx
+++ frontend/src/components/Global/components/ProtectedRoute/protectedroute.jsx
@@ -1,7 +1,23 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 import NotFound from "../../../../views/not-found";
+import axios from "axios";
+import { setSignedIn } from "../../../../slices/user.slice.js";
 
 const ProtectedRoute = ({ children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const check = async () => {
+      const res = await axios.get("/check/check-auth");
+      console.log(res);
+      if (res.data.success) {
+        dispatch(setSignedIn());
+      }
+    };
+    check();
+  }, [dispatch]);
+
   const isAuthenticated = useSelector((state) => state.user.signedIn);
   return isAuthenticated ? <>{children}</> : <NotFound />;
 };
--- frontend/src/views/Dashboard.jsx
+++ frontend/src/views/Dashboard.jsx
@@ -2,24 +2,9 @@ import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import UserProfileBox from "./PatientProfile"; // Import the UserProfileBox component
 import styles from "./Dashboard.module.css";
-import axios from "axios";
-import { setSignedIn } from "../slices/user.slice";
-import { useDispatch } from "react-redux";
 
 const Dashboard = () => {
-  const dispatch = useDispatch();
   const [showUserProfile, setShowUserProfile] = useState(false);
-
-  useEffect(() => {
-    const check = async () => {
-      const res = await axios.get("/check/check-auth");
-      console.log(res);
-      if (res.data.success) {
-        dispatch(setSignedIn());
-      }
-    };
-    check();
-  }, [dispatch]);
 
   const handleUserProfileClick = () => {
     setShowUserProfile(true);
