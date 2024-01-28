567fbc208c588906c30a1281c83922dec1e278ec
Swagat <swagatswaroop@gmail.com>
2024-01-28 16:02:37 +0530

    Update PatientProfile component styling and button appearance

--- frontend/src/views/PatientProfile.jsx
+++ frontend/src/views/PatientProfile.jsx
@@ -7,7 +7,7 @@ const PatientProfile = ({ onClose }) => {
   const [name, setname] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
-  const [profilephoto, setProfilePhoto] = useState("./public/profilephoto.png");
+  const [profilephoto, setProfilePhoto] = useState("./profilephoto.png");
   useEffect(() => {
     const fetchdata = async () => {
       const res = await axios.get("/profile");
@@ -17,7 +17,7 @@ const PatientProfile = ({ onClose }) => {
         setEmail(res.data?.data?.user?.email);
         setPhone(res.data?.data?.user?.phoneno);
         if (res.data?.data?.user?.profilephoto) {
-          setProfilePhoto("res.data?.data?.user?.profilephoto");
+          setProfilePhoto(res.data?.data?.user?.profilephoto);
         }
       }
     };
@@ -25,9 +25,9 @@ const PatientProfile = ({ onClose }) => {
   }, []);
   return (
     <div className="user-profile-box">
-      <h2>User Profile</h2>
-      <div>
-        <img src={profilephoto} alt="" />
+      <div className="profile-field">
+        <h2>User Profile</h2>
+        <img className="rounded-full h-20 w-20" src={profilephoto} alt="" />
       </div>
       <div className="profile-field">
         <label>Name:</label>
@@ -42,7 +42,12 @@ const PatientProfile = ({ onClose }) => {
         <span>{phone}</span>
       </div>
       <div className="profile-field"></div>
-      <button onClick={onClose}>Close</button>
+      <button
+        className="p-3 bg-blue-600 border-none rounded-s-xl cursor-pointer text-xl hover:bg-red-500"
+        onClick={onClose}
+      >
+        Close
+      </button>
     </div>
   );
 };
