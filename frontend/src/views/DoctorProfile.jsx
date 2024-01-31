29f9d05de5480eb07cdac12cf8b9ad97e17a3b81
Swagat <swagatswaroop@gmail.com>
2024-02-01 00:44:10 +0530

    Add address and type fields to DoctorProfile component

--- frontend/src/views/DoctorProfile.jsx
+++ frontend/src/views/DoctorProfile.jsx
@@ -6,6 +6,8 @@ const PatientProfile = ({ onClose }) => {
   const [name, setname] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [type, setType] = useState("");
   const [profilephoto, setProfilePhoto] = useState("./profilephoto.png");
   useEffect(() => {
     const fetchData = async () => {
@@ -17,6 +19,9 @@ const PatientProfile = ({ onClose }) => {
           setname(res.data?.data?.user?.name);
           setEmail(res.data?.data?.user?.email);
           setPhone(res.data?.data?.user?.phoneno);
+          setProfilePhoto(res.data?.data?.user?.profilephoto);
+          setAddress(res.data?.data?.user?.address);
+          setType(res.data?.data?.user?.type);
         }
       } catch (error) {
         alert(error);
@@ -29,7 +34,10 @@ const PatientProfile = ({ onClose }) => {
   return (
     <>
       <div className="user-profile-box">
-        <h2>User Profile</h2>
+        <div className="profile-field">
+          <h2>User Profile</h2>
+          <img className="rounded-full h-20 w-20" src={profilephoto} alt="" />
+        </div>
         <div className="profile-field">
           <label>Name:</label>
           <span>{name}</span>
@@ -39,10 +47,23 @@ const PatientProfile = ({ onClose }) => {
           <span>{email}</span>
         </div>
         <div className="profile-field">
-          <label>Phone no.:</label>
+          <label>Phone no:</label>
           <span>{phone}</span>
         </div>
-        <button onClick={onClose}>Close</button>
+        <div className="profile-field flex-col">
+          <label>Address:</label>
+          <div>{address}</div>
+        </div>
+        <div className="profile-field">
+          <label>Type:</label>
+          <span>{type}</span>
+        </div>
+        <button
+          className="p-3 text-white bg-blue-600 border-none rounded-xl cursor-pointer text-{16px} hover:bg-red-500"
+          onClick={onClose}
+        >
+          Close
+        </button>
       </div>
     </>
   );
