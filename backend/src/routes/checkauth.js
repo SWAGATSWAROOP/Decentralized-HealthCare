8a14110f522c3a99edfe002535324f030a89f08c
Swagat <swagatswaroop@gmail.com>
2024-02-05 18:33:10 +0530

    Remove checkauth route

--- backend/src/routes/checkauth.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import checkAuth from "../controller/check-auth.js";
-
-const router = Router();
-
-router.route("/check-auth").get(checkAuth);
-
-export default router;
