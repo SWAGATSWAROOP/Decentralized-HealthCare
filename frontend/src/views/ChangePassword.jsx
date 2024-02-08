13baf51eeb4cbeb619509cba9c618f4cebd7283e
Swagat <swagatswaroop@gmail.com>
2024-02-08 09:19:29 +0530

    Add password change functionality to ChangePassword component

--- frontend/src/views/ChangePassword.jsx
+++ frontend/src/views/ChangePassword.jsx
@@ -1,6 +1,17 @@
+import { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 
 const ChangePassword = () => {
+  const [oldPass, setOldPass] = useState("");
+  const [newPass, setNewPass] = useState("");
+  const [message, setMessage] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+  useEffect(() => {
+    if (!newPass) return;
+    if (oldPass != newPass) {
+      setMessage("Invalid");
+    } else setMessage("");
+  }, [newPass]);
   return (
     <>
       <div className="flex justify-center">
@@ -9,8 +20,39 @@ const ChangePassword = () => {
           <div className="w-1/2 border-black border-r-2">
             <img src="/forgetpassword.jpg" alt="" />
           </div>
-          <div>
+          <div className="flex justify-center items-center">
             <h1>Change Password</h1>
+            <div className="flex-col">
+              <span>Old Password : </span>
+              <input
+                value={oldPass}
+                onChange={(e) => {
+                  const s = e.target?.value?.trim();
+                  setOldPass(s);
+                }}
+              />
+            </div>
+            <div className="flex-col">
+              <span>New Password : </span>
+              <input
+                value={newPass}
+                onChange={(e) => {
+                  const s = e.target.value.trim();
+                  setNewPass(s);
+                }}
+              />
+            </div>
+            <div className="flex-col">
+              <span>Confirm Password : </span>
+              <input
+                value={confirmPass}
+                onChange={(e) => {
+                  const s = e.target.value.trim();
+                  setConfirmPass(s);
+                }}
+              />
+              <div>{message}</div>
+            </div>
           </div>
         </div>
       </div>
