71b58fbceb23d5d4b779f3550c98887a1ab193e4
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-04-19 00:00:27 +0530

    Docter DashBoard Done

--- frontend/src/views/DDashboard.jsx
+++ frontend/src/views/DDashboard.jsx
@@ -1,17 +1,42 @@
 import DNavBar from "./DNavbar.jsx";
 import styles from "./Dashboard.module.css";
 import { Helmet } from "react-helmet";
+import { useState, useEffect } from "react";
 
 const Dashboard = () => {
+  const [text, setText] = useState("");
+  const openingParagraph =
+    "  Welcome to our healthcare platform! Connect with providers, access records, and prioritize your well-being. Your health, your data, your control!!";
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const intervalId = setInterval(() => {
+      if (currentIndex < openingParagraph.length - 1) {
+        setText((prevText) => prevText + openingParagraph[currentIndex]);
+        currentIndex++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 30);
+
+    return () => {
+      clearInterval(intervalId);
+      setText(openingParagraph);
+    };
+  }, []);
+
   return (
-    <>
+    <div className={styles.dashboardContainer}>
       <Helmet>
-        <title>DashBoard</title>
+        <title>Dashboard</title>
       </Helmet>
-      <div className={styles.dashboardContainer}>
+      <div className="mt-4">
         <DNavBar />
       </div>
-    </>
+      <div className={`${styles.dashboardContent} mt-10`}>
+        <p style={{ lineHeight: "1.5" }}>{text}</p>
+      </div>
+    </div>
   );
 };
 
