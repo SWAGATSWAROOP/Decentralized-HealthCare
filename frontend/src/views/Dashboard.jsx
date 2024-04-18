35191d1a242562603d2de01ea4c32e2b633b800d
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-04-19 01:15:54 +0530

    All Features Are added and project is officially ended

--- frontend/src/views/Dashboard.jsx
+++ frontend/src/views/Dashboard.jsx
@@ -1,10 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { Helmet } from "react-helmet";
 import NavBar from "./NavBar";
 import styles from "./Dashboard.module.css";
+import { userContext } from "../components/Global/components/ProtectedRoute/protectedroute";
+import Web3Modal from "web3modal";
+
+// Importing ABI Contract.
+import AccessRights from "../artifacts/contracts/accessrights.sol/RolesAndRights.json";
+import { ethers } from "ethers";
 
 const Dashboard = () => {
   const [text, setText] = useState("");
+  const { email } = useContext(userContext);
+  const moneyRef = useRef(null);
+
+  async function donateMoney(anon) {
+    const web3modal = new Web3Modal();
+    const connection = await web3modal.connect();
+    const provider = new ethers.BrowserProvider(connection);
+    const signer = await provider.getSigner();
+    const Contract = new ethers.Contract(
+      process.env.REACT_APP_ACCESSRIGHTS_CONTRACT_ADDRESS,
+      AccessRights.abi,
+      signer
+    );
+    const regex = /^[0-9]*\.?[0-9]+$/;
+    if (!regex.test(moneyRef.current.value)) {
+      alert("Enter A valid amount in money");
+      moneyRef.current.value = "";
+      return;
+    }
+    try {
+      const msg = { value: ethers.parseEther(moneyRef.current.value) };
+      const tx = await Contract.donateMoney(email, anon, msg);
+      await tx.wait();
+    } catch (error) {
+      alert("Transaction Rejected");
+    }
+    moneyRef.current.value = "";
+  }
+
   const openingParagraph =
     "  Welcome to our healthcare platform! Connect with providers, access records, and prioritize your well-being. Your health, your data, your control!!";
 
@@ -35,6 +70,24 @@ const Dashboard = () => {
       </div>
       <div className={`${styles.dashboardContent} mt-10`}>
         <p style={{ lineHeight: "1.5" }}>{text}</p>
+      </div>
+      <div className="bg-white mt-4 p-3 text-black">
+        <div>Support the developers by donating</div>
+        <div className="mt-2 flex space-x-2">
+          <input ref={moneyRef} placeholder="Enter the amount" />
+          <button
+            className="p-2 bg-green-300 hover:scale-110"
+            onClick={() => donateMoney(false)}
+          >
+            Donate Money
+          </button>
+          <button
+            className="p-2 bg-green-300 hover:scale-110"
+            onClick={() => donateMoney(true)}
+          >
+            Donate Money Anonmously
+          </button>
+        </div>
       </div>
     </div>
   );
